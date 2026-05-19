988631bc345cfd47ce0314e90b0909c90f42651e
Daniel Fisher <daniel.fisher@climate-x.com>
2026-05-19 11:52:23 +0100

    test: add integration test for missing input file in ingest command

--- cli/tests/integration_test.rs
+++ cli/tests/integration_test.rs
@@ -72,3 +72,15 @@ fn test_cli_resample_missing_input() {
         .failure()
         .stdout(predicate::str::contains("Input database 'missing_input.duckdb' does not exist"));
 }
+
+#[test]
+fn test_cli_ingest_missing_input() {
+    let mut cmd = Command::cargo_bin("zarrduck").unwrap();
+    cmd.arg("ingest")
+        .arg("missing_input.nc")
+        .arg("s3://bucket/out.zarr")
+        .arg("--output=json")
+        .assert()
+        .failure()
+        .stdout(predicate::str::contains("Input file 'missing_input.nc' does not exist"));
+}
